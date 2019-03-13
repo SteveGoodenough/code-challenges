7928ae7e37cb61095e67f6463cce951f1ba7856c
Steve Goodenough <steve.goodenough@johnlewis.co.uk>
2019-03-13 23:09:52 +0000

    first round of tests

--- src/code_challenge_1/challenge_1.py
+++ src/code_challenge_1/challenge_1.py
@@ -1,3 +1,21 @@
 def format_time(s):
-    return '{} {}{}'.format(s,'second','s' if s>1 else '')
+    t = ""
+    if s >= 31536000:
+        u = int(s / 31536000)
+        s = s - u * 31536000
+        t += '{}{} {}{}'.format(' and ' if len(t) else '', u ,'year', 's' if u > 1 else '')
+    if s >= 86400:
+        u = int(s / 86400)
+        s = s - u * 86400
+        t += '{}{} {}{}'.format(' and ' if len(t) else '', u ,'day', 's' if u > 1 else '')
+    if s >= 3600:
+        u = int(s / 3600)
+        s = s - u * 3660
+        t += '{}{} {}{}'.format(' and ' if len(t) else '', u ,'hour', 's' if u > 1 else '')
+    if s >= 60:
+        u = int(s / 60)
+        s = s - u * 60
+        t += '{}{} {}{}'.format(' and ' if len(t) else '', u ,'minute','s' if u > 1 else '')
+    if (s > 0): t += '{}{} {}{}'.format(' and ' if len(t) else '', s, 'second', 's' if s > 1 else '')
+    return t 
 
--- tests/test_formatTime.py
+++ tests/test_formatTime.py
@@ -4,8 +4,38 @@ from code_challenge_1.challenge_1 import format_time
 def test_one_second():
     t = format_time(1)
     assert t == "1 second"
-    
+
+
+def test_two_seconds():
+    t = format_time(2)
+    assert t == "2 seconds"
+
+
 def test_ten_seconds():
     t = format_time(10)
     assert t == "10 seconds"
 
+
+def test_one_minute():
+    t = format_time(60)
+    assert t == "1 minute"
+
+
+def test_one_min_two_secs():
+    t = format_time(62)
+    assert t == "1 minute and 2 seconds"
+
+
+def test_one_hour():
+    t = format_time(3600)
+    assert t == "1 hour"
+
+
+def test_one_day():
+    t = format_time(86400)
+    assert t == "1 day"
+
+
+def test_one_year():
+    t = format_time(31536000)
+    assert t == "1 year"
