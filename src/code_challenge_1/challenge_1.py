52800001dd384e250f040e16b4567fac48354f68
Steve Goodenough <steve.goodenough@johnlewis.co.uk>
2019-03-14 23:13:31 +0000

    working before refactor

--- src/code_challenge_1/challenge_1.py
+++ src/code_challenge_1/challenge_1.py
@@ -1,20 +1,22 @@
 def format_time(seconds):
-    time_string = ""
-    seconds, time_string = time_slice(seconds, time_string, 31536000, 'year')
-    seconds, time_string = time_slice(seconds, time_string, 86400, 'day')
-    seconds, time_string = time_slice(seconds, time_string, 3600, 'hour')
-    seconds, time_string = time_slice(seconds, time_string, 60, 'minute')
-    seconds, time_string = time_slice(seconds, time_string, 1, 'second')
-    return rreplace(time_string.lstrip(", "), ',', ' and', 1)
+        time_string = "none"
+        seconds, time_string = time_slice(seconds, time_string, 31536000, 'year')
+        seconds, time_string = time_slice(seconds, time_string, 86400, 'day')
+        seconds, time_string = time_slice(seconds, time_string, 3600, 'hour')
+        seconds, time_string = time_slice(seconds, time_string, 60, 'minute')
+        seconds, time_string = time_slice(seconds, time_string, 1, 'second')
+        print(time_string)
+        return rreplace(time_string.lstrip(", "), ',', ' and', 1)
 
 
 def time_slice(seconds, time_string, time_unit, unit_name):
-    if seconds >= time_unit:
+    #if seconds >= time_unit:
         units = int(seconds / time_unit)
         seconds = seconds - units * time_unit
-        return seconds, time_string + ', {} {}{}'.format(units, unit_name, 's' if units > 1 else '')
-    else:
-        return seconds, time_string
+        if units > 0:
+            return seconds, (time_string if time_string != 'none' else '') + ', {} {}{}'.format(units, unit_name, 's' if units > 1 else '')
+        else:
+            return seconds, time_string
 
 
 def rreplace(string, old, new, occurrence):
