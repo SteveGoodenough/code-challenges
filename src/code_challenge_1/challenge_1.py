36e840ccef588b079e46ca59d18f3277dadea114
Steve Goodenough <steve.goodenough@johnlewis.co.uk>
2019-03-20 22:50:11 +0000

    Use a list for time slices

--- src/code_challenge_1/challenge_1.py
+++ src/code_challenge_1/challenge_1.py
@@ -1,9 +1,10 @@
 def format_time(seconds):
-    slice = {'year': 31536000, 'day': 86400, 'hour': 3600, 'minute': 60, 'second': 1}
-    time_string = "none"
-    for unit_name, unit_time in slice.items():
+    time_part = {'year': 31536000, 'day': 86400, 'hour': 3600, 'minute': 60, 'second': 1}
+    time_slice = []
+    for unit_name, unit_time in time_part.items():
         units = int(seconds / unit_time)
         if units:
             seconds -= units * unit_time
-            time_string = (time_string if time_string != 'none' else '') + ', {} {}{}'.format(units, unit_name, 's' if units > 1 else '')
-    return ' and'.join(time_string.lstrip(', ').rsplit(',', 1))
+            time_slice.append('{} {}{}'.format(units, unit_name, 's' if units > 1 else ''))
+    time_string = (", ".join(time_slice)) if len(time_slice) else 'none'
+    return ' and'.join(time_string.rsplit(',', 1))
