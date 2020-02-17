0344f2a5b0a84f9df7563bfb613fc35b49d55793
Steve Goodenough <steve.goodenough@johnlewis.co.uk>
2020-02-17 00:34:41 +0000

    Sort issues with deep copies, broken earlier helper function tests

--- src/code_challenge_21/challenge_21.py
+++ src/code_challenge_21/challenge_21.py
@@ -1,41 +1,52 @@
+board = []
 solutions = []
 
 
 def solve_sudoku(grid):
+    global board
     global solutions
     solutions = []
     board = convert_list_to_board(grid)
-    board = solve_it(board)
-    print("exit solutions:", solutions, len(solutions))
+    solve_it()
+    print("solutions found:", len(solutions), solutions)
     return solutions
 
 
-def solve_it(board):
+def solve_it():
+    global board
     print('*', end='')
     for x in range(9):
         for y in range(9):
             if board[y][x] == 0:
                 for num in range(1, 10):
-                    if check_if_number_allowed(board, x, y, num):
+                    if check_if_number_allowed(x, y, num):
                         board[y][x] = num
-                        board = solve_it(board)
+                        solve_it()
                         board[y][x] = 0
-                return board
+                return
     add_solution(board)
-    return board
 
 
 def add_solution(solution):
     global solutions
-    print("solution=", solution)
-    solutions.append(solution)
+    solutions.append(convert_board_to_list(board))
+    print("solution=", solution, len(solutions))
 
 
 def convert_list_to_board(grid):
     return [grid[i:i+9] for i in range(0, len(grid), 9)]
 
 
-def check_if_number_allowed(board, x, y, num):
+def convert_board_to_list(board):
+    flat_list = []
+    for row in board:
+        for col in row:
+            flat_list.append(col)
+    return flat_list
+
+
+def check_if_number_allowed(x, y, num):
+    global board
     for col in range(9):
         if board[y][col] == num:
             return False
